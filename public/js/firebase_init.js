a5ece0cbc3670f8fa8181c0699035d51e4a230e2
Aiman Ismail <aimanismail15@gmail.com>
2019-04-07 15:19:53 -0700

    Accordion buttons in guides now displays database values Updated CSS for index and guides on css style sheets

---
+++ public/js/firebase_init.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyB0eOSPi-RSqClZRIDuwNctzvn1sJlinRw",
+    authDomain: "recylink-42ab4.firebaseapp.com",
+    databaseURL: "https://recylink-42ab4.firebaseio.com",
+    projectId: "recylink-42ab4",
+    storageBucket: "recylink-42ab4.appspot.com",
+    messagingSenderId: "591578593216"
+};
+firebase.initializeApp(config);
