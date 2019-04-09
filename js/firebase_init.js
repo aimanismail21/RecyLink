6d77266693c7124fd70f9539a6532031178c5c1d
Aiman Ismail <46500672+aimanismail21@users.noreply.github.com>
2019-04-08 18:22:39 -0700

    Delete firebase_init.js
    Submission of source files.

--- js/firebase_init.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyB0eOSPi-RSqClZRIDuwNctzvn1sJlinRw",
-    authDomain: "recylink-42ab4.firebaseapp.com",
-    databaseURL: "https://recylink-42ab4.firebaseio.com",
-    projectId: "recylink-42ab4",
-    storageBucket: "recylink-42ab4.appspot.com",
-    messagingSenderId: "591578593216"
-};
-firebase.initializeApp(config);
