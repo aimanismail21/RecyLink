b4b6e3f0717ace5c1859aa04b4a3e32acf2e5cc9
Aiman Ismail <46500672+aimanismail21@users.noreply.github.com>
2019-04-08 22:06:12 -0700

    Slight revision of code in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 //Write into Give Your Feedback database
-
+i = 0;
 function submitClick(){
     var name = document.getElementById("name");
     var email = document.getElementById("email");
     var message = document.getElementById("message");
-    variable = email.value;
-    
+    variable = name.value;
     firebase.database().ref("UserMsgs/" + variable).set({
         name: name.value,
         email: email.value,
         message: message.value
             });
-        } 
+        }
+
    
